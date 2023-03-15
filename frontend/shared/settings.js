871b718a10dfcd70a55fcf896d76ec5bb846c4c1
joemark0008 <41988621+joemark0008@users.noreply.github.com>
2023-03-15 22:19:44 +0800

    api updates

--- frontend/shared/settings.js
+++ frontend/shared/settings.js
@@ -4,8 +4,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   apiUrlPrefix = "http://localhost:5827";
 } else {
   if (process.browser) {
-    apiUrlPrefix = "http://localhost:5827";
+    apiUrlPrefix = "https://api-construction.devcraftstudio.online";
   } else {
-    apiUrlPrefix = "http://localhost:5827";
+    apiUrlPrefix = "https://api-construction.devcraftstudio.online";
   }
 }
