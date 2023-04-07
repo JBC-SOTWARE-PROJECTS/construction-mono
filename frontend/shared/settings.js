04e451faf829ccb6cb9b578b034eae9b43136af4
joemark0008 <41988621+joemark0008@users.noreply.github.com>
2023-04-07 12:53:43 +0800

    deployment argo

--- frontend/shared/settings.js
+++ frontend/shared/settings.js
@@ -4,8 +4,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   apiUrlPrefix = "http://localhost:5827";
 } else {
   if (process.browser) {
-    apiUrlPrefix = "https://api-demo.devcraftstudio.online";
+    apiUrlPrefix = "https://construction-api.devcraftstudio.online";
   } else {
-    apiUrlPrefix = "https://api-demo.devcraftstudio.online";
+    apiUrlPrefix = "https://construction-api.devcraftstudio.online";
   }
 }
