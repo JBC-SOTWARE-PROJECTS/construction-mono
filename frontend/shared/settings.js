cfbcb2b39412d134cba93c0bad59ea870a03de14
wilsonJosephHinacay <pangetnacu@gmail.com>
2023-05-05 12:29:42 +0800

    build

--- frontend/shared/settings.js
+++ frontend/shared/settings.js
@@ -4,8 +4,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   apiUrlPrefix = "http://localhost:5827";
 } else {
   if (process.browser) {
-    apiUrlPrefix = "https://construction-api.devcraftstudio.online";
+    apiUrlPrefix = "https://api-inventory.megaptk.com";
   } else {
-    apiUrlPrefix = "https://construction-api.devcraftstudio.online";
+    apiUrlPrefix = "https://api-inventory.megaptk.com";
   }
 }
