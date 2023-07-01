1930a64ba5f3f1219cc3c79bf7d79265ff194121
wilsonJosephHinacay <pangetnacu@gmail.com>
2023-07-01 09:34:25 +0800

    updates

--- backend/src/main/resources/config/liquibase/changelog3/16_gasoline_type.sql
+++ backend/src/main/resources/config/liquibase/changelog3/16_gasoline_type.sql
@@ -12,11 +12,30 @@ CREATE TABLE asset.gas_type (
 	deleted bool NULL
 );
 
-CREATE TABLE asset.gas_ledger (
+CREATE TABLE asset.repair_history (
     id                  uuid not null primary key,
-    ref_no                varchar,
+    ref_no              varchar,
     description         varchar,
-    active              bool,
+    project             uuid null,
+    asset               uuid null,
+    remarks             varchar,
+    status              varchar,
+
+    created_by          varchar(50) NULL,
+	created_date        timestamp NULL DEFAULT now(),
+	last_modified_by    varchar(50) NULL,
+	last_modified_date  timestamp NULL DEFAULT now(),
+	deleted bool NULL
+);
+
+CREATE TABLE asset.repair_details (
+    id                  uuid not null primary key,
+    date_transact       timestamp NULL DEFAULT now(),
+    description         varchar,
+    item                uuid null,
+    repair_history      uuid null,
+    qty                 int default 0,
+    cost                numeric default 0,
 
     created_by          varchar(50) NULL,
 	created_date        timestamp NULL DEFAULT now(),
