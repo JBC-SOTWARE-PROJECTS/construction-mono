efb10ab02ec1d740b2546a945ef2dfab45e0e443
Jacky <jackyavenido.se@gmail.com>
2024-01-05 06:18:52 +0800

    remove sample data auth

--- backend/src/main/resources/config/liquibase/sample_data/1_5_2024_public_user_authorities.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.t_user_authority (user_id, authority_name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO public.t_user_authority (user_id, authority_name) VALUES (1, 'ROLE_USER');
-INSERT INTO public.t_user_authority (user_id, authority_name) VALUES (1, 'ROLE_STAFF');
