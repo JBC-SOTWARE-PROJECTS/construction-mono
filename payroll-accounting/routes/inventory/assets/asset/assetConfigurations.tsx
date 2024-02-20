0d4c516a432ded34f3e07fbe6989e9d0773c557c
Jacky <jackyavenido.se@gmail.com>
2024-02-21 05:36:45 +0800

    rental migration

---
+++ payroll-accounting/routes/inventory/assets/asset/assetConfigurations.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const AssetConfigurations = (props: Props) => {
+  return (
+    <div>AssetConfigurations</div>
+  )
+}
+
+export default AssetConfigurations
