2abf1d0cec94672ef0ebf7e24df74af895e2b0e7
Jacky <jackyavenido.se@gmail.com>
2024-03-03 23:05:29 +0800

    permissions and view

--- payroll-accounting/components/inventory/assets/dialogs/upsertVehicleUsage.tsx
+++ payroll-accounting/components/inventory/assets/dialogs/upsertVehicleUsage.tsx
@@ -41,11 +41,13 @@ import { IPMState } from "./vehicleUsageAttachment";
 import useGetRentalRateByAsset from "@/hooks/asset/useGetRentalRateByAsset";
 import { currencyDisplay } from "@/shared/settings";
 import FormTextArea from "@/components/common/formTextArea/formTextArea";
+import AccessControl from "@/components/accessControl/AccessControl";
 
 interface IProps {
   hide: (hideProps: any) => void;
   record?: VehicleUsageMonitoring | null | undefined;
   projectOpts: any;
+  viewModeSet : boolean
 }
 
 const defRec = {
@@ -58,7 +60,7 @@ const defRec = {
   startFuelReading: null,
   endFuelReading: null,
   projectId: null,
-  rentalBasisId: null
+  rentalBasisId: null,
 };
 
 const initialState: IPMState = {
@@ -68,7 +70,7 @@ const initialState: IPMState = {
 };
 
 export default function UpsertVehicleUsageModal(props: IProps) {
-  const { hide, record, projectOpts } = props;
+  const { hide, record, projectOpts, viewModeSet } = props;
   const [showPasswordConfirmation] = ConfirmationPasswordHook();
   const [initRecord, setinitRecord] = useState<VehicleUsageMonitoring | null>(
     null
@@ -76,9 +78,11 @@ export default function UpsertVehicleUsageModal(props: IProps) {
   const [selectedEmps, setSelectedEmps] = useState<VehicleUsageEmployee[]>([]);
   const [deletedEmps, setDeletedEmps] = useState<string[]>([]);
   const [statePage, setState] = useState(initialState);
+  const [viewMode, setViewMode] = useState(viewModeSet ?? true);
   const [calculatedRental, setCalculatedRental] = useState(0);
   const [form] = Form.useForm();
   const router = useRouter();
+
   moment.locale("en");
 
   const [asset, loadingAsset] = useGetAssetById(router?.query?.id);
@@ -110,11 +114,14 @@ export default function UpsertVehicleUsageModal(props: IProps) {
       initRec.startDatetime = dayjs(initRec.startDatetime ?? new Date());
       initRec.endDatetime = dayjs(initRec.endDatetime ?? new Date());
       initRec.projectId = record?.project ? record?.project?.id : null;
-      initRec.rentalBasisId = record?.rentalBasis ? record?.rentalBasis?.id : null;
+      initRec.rentalBasisId = record?.rentalBasis
+        ? record?.rentalBasis?.id
+        : null;
 
       setinitRecord(initRec);
-      setCalculatedRental((initRec?.rentalRate ??0)*(initRec?.rentUnitMeasureQuantity ??0))
-   
+      setCalculatedRental(
+        (initRec?.rentalRate ?? 0) * (initRec?.rentUnitMeasureQuantity ?? 0)
+      );
     } else {
       setinitRecord(defRec);
     }
@@ -224,27 +231,33 @@ export default function UpsertVehicleUsageModal(props: IProps) {
 
   const handleRentRateChange = () => {
     const fieldsValue = form.getFieldsValue();
-    setCalculatedRental((fieldsValue?.rentUnitMeasureQuantity ?? 0) * (fieldsValue?.rentalRate ?? 0));
+    setCalculatedRental(
+      (fieldsValue?.rentUnitMeasureQuantity ?? 0) *
+        (fieldsValue?.rentalRate ?? 0)
+    );
   };
 
   const handleRentalBasisChange = () => {
     const fieldsValue = form.getFieldsValue();
-    const rentBasis : RentalRates[]= dataRentalRates?.content.filter((record: RentalRates)=>record?.id == fieldsValue?.rentalBasisId)
-  
+    const rentBasis: RentalRates[] = dataRentalRates?.content.filter(
+      (record: RentalRates) => record?.id == fieldsValue?.rentalBasisId
+    );
+
     form.setFieldsValue({
       rentalRate: rentBasis[0]?.amount,
     });
 
-    setCalculatedRental((fieldsValue?.rentUnitMeasureQuantity ?? 0) * (rentBasis[0]?.amount))
-   
+    setCalculatedRental(
+      (fieldsValue?.rentUnitMeasureQuantity ?? 0) * rentBasis[0]?.amount
+    );
   };
 
   var rentUnitOpts = dataRentalRates?.content.map((item: RentalRates) => {
     return {
       value: item?.id,
-      label: `${currencyDisplay} ${parseFloat(item.amount).toFixed(
-        2
-      )} per ${item.measurement ?? 0} ${item.unit ?? ""}`,
+      label: `${currencyDisplay} ${parseFloat(item.amount).toFixed(2)} per ${
+        item.measurement ?? 0
+      } ${item.unit ?? ""}`,
     };
   });
 
@@ -252,9 +265,9 @@ export default function UpsertVehicleUsageModal(props: IProps) {
     <Modal
       title={
         <Typography.Title level={4}>
-          <Space align="center">{`${
-            record?.id ? "Edit" : "Add"
-          } Vehicle Usage`}</Space>
+          <Space align="center">{`
+          ${viewMode ? "" : `${record?.id ? "Edit" : "Add"} `}
+          Vehicle Usage`}</Space>
         </Typography.Title>
       }
       destroyOnClose={true}
@@ -264,18 +277,22 @@ export default function UpsertVehicleUsageModal(props: IProps) {
       style={{ maxWidth: "1300px" }}
       onCancel={() => hide(false)}
       footer={
-        <Space>
-          <Button
-            type="primary"
-            size="large"
-            htmlType="submit"
-            form="upsertVehicleUsage"
-            loading={upsertLoading}
-            icon={<SaveOutlined />}
-          >
-            {`${record?.id ? "Save Changes" : "Save"} & Close`}
-          </Button>
-        </Space>
+        viewMode == false ? (
+          <Space>
+            <Button
+              type="primary"
+              size="large"
+              htmlType="submit"
+              form="upsertVehicleUsage"
+              loading={upsertLoading}
+              icon={<SaveOutlined />}
+            >
+              {`${record?.id ? "Save Changes" : "Save"} & Close`}
+            </Button>
+          </Space>
+        ) : (
+          <></>
+        )
       }
     >
       <>
@@ -289,6 +306,7 @@ export default function UpsertVehicleUsageModal(props: IProps) {
             initialValues={{
               ...initRecord,
             }}
+            disabled={viewMode ? true : false}
           >
             <Row gutter={[8, 0]}>
               <Col span={12}>
@@ -405,99 +423,115 @@ export default function UpsertVehicleUsageModal(props: IProps) {
 
             {record ? (
               <>
-                <Divider orientation="left">EMPLOYEE INVOLVEMENT</Divider>
-                <EmployeeDrawer
-                  selectedEmployees={employeeList}
-                  loading={false}
-                  usage="MULTI"
-                  onSelect={(selected: Employee[]) => {
-                    const elementExists = _.filter(
-                      selectedEmps,
-                      (emp) => emp.employee?.id == selected[0].id
-                    );
-
-                    if (elementExists.length == 0) {
-                      dayjs.locale("en");
-                      var recEmp: VehicleUsageEmployee[] = selected.map(
-                        (rec: Employee) => ({
-                          employee: rec,
-                          company: initRecord.company,
-                          item: initRecord.item,
-                          asset: initRecord.asset,
-                          vehicleUsage: initRecord.id,
-                          designation: "DRIVER",
-                          timeRenderedEnd: dayjs(new Date()).format(
-                            "MMMM D, YYYY, h:mm:ss A"
-                          ),
-                          timeRenderedStart: dayjs(new Date()).format(
-                            "MMMM D, YYYY, h:mm:ss A"
-                          ),
-                          remarks: "N/A",
-                        })
-                      );
-
-                      setSelectedEmps(_.concat(selectedEmps, recEmp));
-                    }
-                  }}
+                <AccessControl
+                  allowedPermissions={["manage_employee_on_vehicle_usage"]}
                 >
-                  <>Select Employee</>
-                </EmployeeDrawer>
-                <VehicleUsageEmployeeTable
-                  selectedEmps={selectedEmps}
-                  handleSelected={handleSelected}
-                  handleDeleted={handleDeleted}
-                />
+                  <Divider orientation="left">EMPLOYEE INVOLVEMENT</Divider>
+                  {viewMode == false ? (
+                    <>
+                      <EmployeeDrawer
+                        selectedEmployees={employeeList}
+                        loading={false}
+                        usage="MULTI"
+                        onSelect={(selected: Employee[]) => {
+                          const elementExists = _.filter(
+                            selectedEmps,
+                            (emp) => emp.employee?.id == selected[0].id
+                          );
 
-                <Divider orientation="left">RENT DETAILS</Divider>
-                <Row gutter={[8, 0]}>
-                  <Col span={12}>
-                     <FormSelect
-                      name="rentalBasisId"
-                      label="Rental Basis"
-                      propsselect={{
-                        options: rentUnitOpts,
-                        allowClear: true,
-                        placeholder: "Select rental basis",
-                        onChange: handleRentalBasisChange
-                      }}
-                    />
-                  </Col>
-                  <Col span={3}>
-                    <FormInput
-                      name="rentUnitMeasureQuantity"
-                      label="Quantity"
-                      propsinput={{
-                        defaultValue: 0,
-                        type: "number",
-                        placeholder: "",
-                        onChange: handleRentRateChange
-                      }}
-                    />
-                  </Col>
-                  <Col span={3}>
-                    <FormInput
-                      name="rentalRate"
-                      label="Rental Rate"
-                      propsinput={{
-                        defaultValue: 0,
-                        type: "number",
-                        placeholder: "",
-                        onChange: handleRentRateChange
-                      }}
-                    />
-                  </Col>
-                  <Col span={4}>
-                    <FormInput
-                      name="calculatedRentalFee"
-                      label="Calculated Rental Fee"
-                      propsinput={{
-                        disabled: true,
-                        type: "number",
-                        placeholder: (calculatedRental).toString(),
-                      }}
-                    />
-                  </Col>
-                </Row>
+                          if (elementExists.length == 0) {
+                            dayjs.locale("en");
+                            var recEmp: VehicleUsageEmployee[] = selected.map(
+                              (rec: Employee) => ({
+                                employee: rec,
+                                company: initRecord.company,
+                                item: initRecord.item,
+                                asset: initRecord.asset,
+                                vehicleUsage: initRecord.id,
+                                designation: "DRIVER",
+                                timeRenderedEnd: dayjs(new Date()).format(
+                                  "MMMM D, YYYY, h:mm:ss A"
+                                ),
+                                timeRenderedStart: dayjs(new Date()).format(
+                                  "MMMM D, YYYY, h:mm:ss A"
+                                ),
+                                remarks: "N/A",
+                              })
+                            );
+
+                            setSelectedEmps(_.concat(selectedEmps, recEmp));
+                          }
+                        }}
+                      >
+                        <>Select Employee</>
+                      </EmployeeDrawer>
+                    </>
+                  ) : (
+                    <></>
+                  )}
+
+                  <VehicleUsageEmployeeTable
+                    viewMode={viewMode}
+                    selectedEmps={selectedEmps}
+                    handleSelected={handleSelected}
+                    handleDeleted={handleDeleted}
+                  />
+                </AccessControl>
+
+                <AccessControl
+                  allowedPermissions={["manage_employee_on_vehicle_usage"]}
+                >
+                  <Divider orientation="left">RENT DETAILS</Divider>
+                  <Row gutter={[8, 0]}>
+                    <Col span={12}>
+                      <FormSelect
+                        name="rentalBasisId"
+                        label="Rental Basis"
+                        propsselect={{
+                          options: rentUnitOpts,
+                          allowClear: true,
+                          placeholder: "Select rental basis",
+                          onChange: handleRentalBasisChange,
+                        }}
+                      />
+                    </Col>
+                    <Col span={3}>
+                      <FormInput
+                        name="rentUnitMeasureQuantity"
+                        label="Quantity"
+                        propsinput={{
+                          defaultValue: 0,
+                          type: "number",
+                          placeholder: "",
+                          onChange: handleRentRateChange,
+                        }}
+                      />
+                    </Col>
+                    <Col span={3}>
+                      <FormInput
+                        name="rentalRate"
+                        label="Rental Rate"
+                        propsinput={{
+                          defaultValue: 0,
+                          type: "number",
+                          placeholder: "",
+                          onChange: handleRentRateChange,
+                        }}
+                      />
+                    </Col>
+                    <Col span={4}>
+                      <FormInput
+                        name="calculatedRentalFee"
+                        label="Calculated Rental Fee"
+                        propsinput={{
+                          disabled: true,
+                          type: "number",
+                          placeholder: calculatedRental.toString(),
+                        }}
+                      />
+                    </Col>
+                  </Row>
+                </AccessControl>
               </>
             ) : (
               <></>
