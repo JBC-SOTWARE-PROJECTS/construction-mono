ddaf811d3e207ea2fd4e48dba6f6c845481aa7b5
09jeriah <aruts.jeriah@gmail.com>
2024-04-15 17:55:11 +0800

    Payroll Register Per Proj

---
+++ backend/src/main/groovy/com/backend/gbp/rest/report/PayrollPerProject.groovy
@@ -0,0 +1,203 @@
+package com.backend.gbp.rest.report
+
+import com.backend.gbp.domain.hrm.Employee
+import com.backend.gbp.domain.payroll.Payroll
+import com.backend.gbp.domain.payroll.PayrollEmployee
+import com.backend.gbp.domain.payroll.TimekeepingEmployee
+import com.backend.gbp.graphqlservices.payroll.TimekeepingEmployeeService
+import com.backend.gbp.repository.hrm.EmployeeRepository
+import com.backend.gbp.repository.payroll.PayrollEmployeeRepository
+import com.backend.gbp.repository.payroll.PayrollRepository
+import groovy.transform.Canonical
+import groovy.transform.TypeChecked
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+import java.math.RoundingMode
+import java.nio.charset.Charset
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.Callable
+
+
+@Canonical
+class PayrollPerRegDto {
+    String companyName;
+    String projectName;
+    String payrollCode;
+    String name;
+    String hours;
+    String grossPay;
+}
+
+@Canonical
+class HrsDto{
+   String empName;
+   BigDecimal hours;
+   BigDecimal grossPay;
+
+}
+
+@Canonical
+class  PayrollPerProj{
+    UUID id;
+    String projectName;
+}
+
+
+
+@TypeChecked
+@RestController
+@RequestMapping('/reports/payroll/print')
+class PayrollPerProject {
+
+    @Autowired
+    PayrollEmployeeRepository payrollEmployeeRepository
+
+    @Autowired
+    PayrollRepository payrollRepository
+
+    @Autowired
+    EmployeeRepository employeeRepository
+
+
+
+    @RequestMapping("/payrollPerRegister")
+    Callable<ResponseEntity<byte[]>> payrollPerRegister(
+            @RequestParam(name ="id") UUID id
+
+    ) {
+        Map<UUID, Employee> employeeMap = [:]
+        Map<UUID, BigDecimal> prjTotal = [:]
+
+        Map<UUID, PayrollPerProj> projGroup = [:]
+        Map<UUID, Map<UUID,HrsDto>> empPrjHours = [:]
+        def payroll = payrollRepository.findById(id).get()
+
+        Instant currentInstant = Instant.now();
+        ZonedDateTime zonedDateTime = currentInstant.atZone(ZoneId.of("UTC"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDate = formatter.format(zonedDateTime);
+        DateTimeFormatter formatterD = DateTimeFormatter.ofPattern("MM-dd-yyyy").withZone(ZoneId.systemDefault())
+        String startDate = formatterD.format(payroll?.dateStart)
+        String endDate = formatterD.format(payroll?.dateEnd)
+
+        return new Callable<ResponseEntity<byte[]>>() {
+            @Override
+            ResponseEntity<byte[]> call() throws Exception {
+                StringBuffer buffer = new StringBuffer()
+                CSVPrinter csvPrinter = new CSVPrinter(buffer, CSVFormat.POSTGRESQL_CSV)
+
+                csvPrinter.printRecord("Company Name", payroll.company.companyName ?: "")
+                csvPrinter.printRecord("Payroll Code", payroll.code ?: "")
+                csvPrinter.printRecord("PayPeriod from ", "${startDate} to ${endDate}")
+
+                BigDecimal totalPay = 0.0
+
+                if (payroll) {
+                    def timekeeping = payroll?.timekeeping;
+                    def allowance = payroll?.allowance?.allowanceEmployees;
+
+                    BigDecimal allowanceItm = 0.0
+                    allowance.each {al ->
+                        al.allowanceItems.each{
+                            allowanceItm += it.amount ?: 0.0
+                        }
+                    }
+
+                    timekeeping.timekeepingEmployees.each {
+                        it.accumulatedLogs?.each { al ->
+                            def employee = new Employee()
+                            if (employeeMap[al?.employeeId]) {
+
+                                employee = employeeMap[al?.employeeId]
+                            } else {
+                                employee = employeeRepository.findById(al?.employeeId).get()
+                                employeeMap[al?.employeeId] = employee
+                            }
+
+                            al.projectBreakdown.each {
+                                hl ->
+
+                                    if (!projGroup[hl.project]) {
+                                        PayrollPerProj proj =  new PayrollPerProj()
+                                        proj.id = hl.project
+                                        proj.projectName = hl.projectName
+                                        projGroup[hl.project] = proj
+                                        prjTotal[hl.project] = 0.00
+                                    }
+
+                                    if(empPrjHours[hl.project]){
+                                        if(empPrjHours[hl.project][al?.employeeId]){
+                                            empPrjHours[hl.project][al?.employeeId].hours += hl?.totalRegularHours ?: 0
+                                        }
+                                        else {
+                                            HrsDto hrs = new HrsDto()
+                                            BigDecimal gross = (hl.regular ?: BigDecimal.ZERO) + (hl.overtime ?: BigDecimal.ZERO) + (hl.regularHoliday ?: BigDecimal.ZERO)
+                                            gross = gross.setScale(2, RoundingMode.HALF_UP)
+                                            BigDecimal totalGrossPay = gross + allowanceItm
+
+                                            hrs.empName = employee?.fullName
+                                            hrs.hours = hl?.totalRegularHours
+                                            hrs.grossPay = totalGrossPay
+                                            empPrjHours[hl.project][al?.employeeId] = hrs
+                                            prjTotal[hl.project] = prjTotal[hl.project] + totalGrossPay
+                                        }
+                                    }else {
+                                        Map<UUID,HrsDto> hrsDtoMap = [:]
+                                        HrsDto hrs = new HrsDto()
+                                        hrs.empName = employee?.fullName
+                                        hrs.hours = hl?.totalRegularHours
+                                        hrsDtoMap[al?.employeeId] = hrs
+                                        empPrjHours[hl.project] = hrsDtoMap
+                                    }
+
+                            }
+                        }
+                    }
+
+                    BigDecimal AllTotal = 0.00
+
+                    projGroup.each {
+                        project, projInfo ->
+                            csvPrinter.printRecord("")
+                            csvPrinter.printRecord("")
+                            csvPrinter.printRecord( projInfo?.projectName ?: "")
+                            csvPrinter.printRecord("Name", "Hours", "Gross Pay")
+                            csvPrinter.printRecord("")
+
+                            empPrjHours[project].each {
+                                empId, hrs ->
+                                    csvPrinter.printRecord(hrs.empName, Math.round(hrs.hours as double), hrs.grossPay);
+                            }
+
+                            csvPrinter.printRecord("")
+                            csvPrinter.printRecord("")
+                            csvPrinter.printRecord("Total Pay", prjTotal[project])
+                            AllTotal +=prjTotal[project]
+                    }
+
+                    csvPrinter.printRecord("Total Pay for Company", AllTotal)
+                }
+
+                def data = buffer.toString().getBytes(Charset.defaultCharset())
+                def responseHeaders = new HttpHeaders()
+                responseHeaders.setContentType(MediaType.TEXT_PLAIN)
+                responseHeaders.setContentLength(data.length)
+                responseHeaders.add("Content-Disposition", "attachment;filename=payrollPerRegister.csv")
+                return new ResponseEntity(data, responseHeaders, HttpStatus.OK)
+
+            }
+        }
+    }
+}
--- payroll-accounting/routes/payroll/payroll-management/p-payslip.tsx
+++ payroll-accounting/routes/payroll/payroll-management/p-payslip.tsx
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Button } from 'antd';
-import { ArrowLeftOutlined } from '@ant-design/icons';
-import { useRouter } from 'next/router';
-import { PageContainer, ProCard } from '@ant-design/pro-components';
+import { Button } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { useRouter } from "next/router";
+import { PageContainer, ProCard } from "@ant-design/pro-components";
 
-import PayslipSearchForm from './p-form/payslip-search-form';
-import { useQuery } from '@apollo/client';
-import { GET_ALL_PAYROLL_EMPLOYEE } from '@/graphql/company/queries';
+import PayslipSearchForm from "./p-form/payslip-search-form";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_PAYROLL_EMPLOYEE } from "@/graphql/company/queries";
 import useGetPayrollEmployeesPageable, {
   variables,
-} from '@/hooks/payroll/useGetPayrollEmployeesPageable';
-
-// const initialState: IState = {
-//   filter: '',
-//   status: true,
-//   page: 0,
-//   size: 10,
-//   office: null,
-//   position: null,
-// };
+} from "@/hooks/payroll/useGetPayrollEmployeesPageable";
 
 const initialState: variables = {
-  filter: '',
+  filter: "",
   size: 25,
   page: 0,
   status: [],
@@ -31,23 +22,12 @@ const initialState: variables = {
 function PayrollPayslip() {
   const router = useRouter();
   const [state, setState] = useState(initialState);
-  const [selectedEmp, setSelectedEmp] = useState<any[]>([]);
-  const [viewEmp, setViewEmp] = useState<any>();
 
   const { data, loading, refetch } = useQuery(GET_ALL_PAYROLL_EMPLOYEE, {
     variables: {
       id: router?.query?.id,
     },
   });
-
-  // const { data: payrollEmp } = useQuery(FETCH_PAYROLL_EMP, {
-  //   variables: {
-  //     filter: state.filter,
-  //     page: state.page,
-  //     pageSize: state.size,
-  //     payrollId: router?.query?.id,
-  //   },
-  // });
 
   const [employees, loadingPayrollEmployees, totalElements] =
     useGetPayrollEmployeesPageable({ variables: state });
@@ -58,7 +38,7 @@ function PayrollPayslip() {
         title={
           <>
             <Button
-              size='middle'
+              size="middle"
               onClick={() => router.back()}
               icon={<ArrowLeftOutlined />}
             >
@@ -69,17 +49,16 @@ function PayrollPayslip() {
       >
         <ProCard
           headStyle={{
-            flexWrap: 'wrap',
+            flexWrap: "wrap",
           }}
           bordered
           headerBordered
         >
           <PayslipSearchForm
-            setSelectedEmp={setSelectedEmp}
-            viewEmp={setViewEmp}
             loading={loadingPayrollEmployees}
             data={employees || []}
             filter={state.filter}
+            onSearch={(value) => setState({ ...state, filter: value })}
           />
         </ProCard>
       </PageContainer>
